7ac8bed79d6d020e17fd1e812febdac9e7523ca9
austinaeden <austin.mbogo@student.moringaschool.com>
2023-09-05 20:32:52 +0300

    update

--- lib/app.py
+++ lib/app.py
@@ -20,6 +20,45 @@ class Customer(Base):
     reviews = relationship("Review", back_populates="customer")
     restaurants=relationship("Review",back_populates="customer",overlaps="reviews")
 
+    def __repr__(self):
+        return f"name: {self.first_name} {self.last_name}"
+    
+    def cus_reviews(self):
+        reviews=session.query(Review).filter_by(customer_id=self.cus_id).all()
+        return reviews
+    
+    def cust_restaurants(self):
+        reviews=session.query(Review).filter_by(customer_id=self.cus_id).all()
+        return [review.restaurant for review in reviews]
+
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def favorite_restaurant(self):
+        review=session.query(Review).filter_by(customer_id=self.cus_id).order_by(Review.star_rating.desc()).limit(1).first()
+        return review.restaurant
+    
+    def add_review(self, restaurant, rating):
+        new_review=Review(
+            star_rating=rating,
+            restaurant_id=restaurant,
+            customer=self
+        )
+        session.add(new_review)
+        session.commit()
+
+    def delete_review(self, restaurant):
+        print(f"Deleting reviews for customer_id={self.cus_id} and restaurant_id={restaurant}")
+        reviews = session.query(Review).filter_by(customer_id=self.cus_id, restaurant_id=restaurant).all()
+
+        for review in reviews:
+            print(f"Deleting with id {review.cus_id}")
+            session.delete(review)
+        
+        session.commit()
+
+        return "reviews deleted"
+
 class Restaurant (Base):
     __tablename__='restaurant'
     res_id = Column(Integer, Sequence('res_id_seq'), primary_key=True)
@@ -27,6 +66,26 @@ class Restaurant (Base):
     res_price = Column(Integer)
 
     reviews = relationship("Review", back_populates="restaurant")
+
+    def __repr__(self):
+        return f"name:{self.res_name}, price:{self.res_price}"
+    
+    def res_customers(self):
+        reviews=session.query(Review).filter_by(restaurant_id=self.res_id).all()
+        return [review.customer for review in reviews]
+    
+    def res_reviews(self):
+        reviews=session.query(Review).filter_by(restaurant_id=self.res_id).all()
+        return reviews
+    
+    @classmethod
+    def fanciest_reataurant(cls):
+        restaurant = session.query(Restaurant).order_by(Restaurant.res_price.desc()).first()
+        return restaurant
+    
+    def all_reviews(self):
+        reviews = session.query(Review).filter_by(restaurant_id=self.id).all()
+        return [review.full_review() for review in reviews]
 
 
 class Review (Base):
@@ -38,6 +97,20 @@ class Review (Base):
 
     restaurant = relationship("Restaurant", back_populates="reviews")
     customer = relationship("Customer", back_populates="reviews")
+
+    def rev_customer(self):
+        return self.customer
+    
+    def rev_restaurant(self):
+        return self.restaurant
+    
+    def __repr__(self):
+        return f"Star rating{self.star_rating}, customer id:{self.cus_id}, restaurant id:{self.res_id}"
+    
+    def full_review(self):
+        return f"Review for {self.restaurant.res_name} by {self.customer.full_name()}  : {self.star_rating} stars"
+
+
 
 
 # create session
---
+++ lib/migrations/versions/8052d4c3a07e_empty_init.py
@@ -0,0 +1,24 @@
+"""empty init
+
+Revision ID: 8052d4c3a07e
+Revises: b627d79120d8
+Create Date: 2023-09-05 15:42:43.776707
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '8052d4c3a07e'
+down_revision = 'b627d79120d8'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
